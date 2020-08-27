4e84c6972eba78fa0423360ba8e8f55a1883f581
enshck <ernest.jewatov@gmail.com>
2020-08-27 18:40:39 +0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="hello">
+    <h1 v-for="message in messages" :key="message.id">{{ message }}</h1>
+    <div @click="randomSort" class="submitButton">Random</div>
+    <form class="inputsContainer">
+      <input
+        v-for="inputData in formsInputs"
+        v-model="formData[inputData.name]"
+        :key="inputData.id"
+        :type="inputData.type"
+      />
+      <div v-on:click="submitButton" class="submitButton">Submit</div>
+      <div>{{ipAddress}}</div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { formsInputs } from "../constants";
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data: () => ({
+    messages: [
+      "Hello world-0",
+      "Hello world-1",
+      "Hello world-2",
+      "Hello world-3",
+      "Hello world-4",
+    ],
+    formData: {
+      name: "",
+      surname: "",
+      email: "",
+      password: "",
+      age: "",
+    },
+    formsInputs,
+  }),
+  methods: {
+    randomSort: function () {
+      this.messages = this.messages.sort(() => 0.5 - Math.random());
+    },
+    submitButton: function () {
+      const formData = { ...this.formData };
+      console.log(formData, "dat");
+    },
+  },
+  computed: {
+    ipAddress: function () {
+      return "10.10.10.10";
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.submitButton {
+  border: 1px solid red;
+  max-width: 200px;
+  margin: 0 auto;
+  padding: 10px;
+  cursor: pointer;
+  user-select: none;
+}
+.inputsContainer {
+  display: flex;
+  flex-direction: column;
+  max-width: 200px;
+  margin: 20px auto;
+}
+
+.inputsContainer input {
+  margin-top: 10px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const formsInputs = [
+  {
+    type: "text",
+    name: "name",
+  },
+  {
+    type: "text",
+    name: "surname",
+  },
+  {
+    type: "email",
+    name: "email",
+  },
+  {
+    type: "password",
+    name: "password",
+  },
+  {
+    type: "date",
+    name: "age",
+  },
+];
